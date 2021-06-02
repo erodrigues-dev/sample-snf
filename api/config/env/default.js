308eb1e890c447f54dfc136468ff9db4f18e64dc
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-02 13:39:20 -0300

    chore: add mongodb configuration

--- api/config/env/default.js
+++ api/config/env/default.js
@@ -1,59 +1,63 @@
 module.exports = {
-    "app": {
-        "name": "todo-list",
-        "baseRoute": "/api",
-        "port": 8091
+  app: {
+    name: "todo-list",
+    baseRoute: "/api",
+    port: 8091,
+  },
+  cors: {
+    preflightMaxAge: 5,
+    origins: ["*"],
+    allowHeaders: [
+      "x-origin-channel",
+      "x-origin-application",
+      "x-origin-device",
+      "x-identifier",
+    ],
+    exposeHeaders: [],
+  },
+  log: {
+    debug: true,
+    bunyan: {
+      name: "Application",
+      streams: [
+        {
+          level: "debug",
+          type: "rotating-file",
+          path: "logs/{hostname}.log",
+          period: "1d",
+          count: 2,
+        },
+      ],
     },
-    "cors": {
-        "preflightMaxAge": 5,
-        "origins": [
-            "*"
-        ],
-        "allowHeaders": [
-            "x-origin-channel",
-            "x-origin-application",
-            "x-origin-device",
-            "x-identifier"
-        ],
-        "exposeHeaders": []
+  },
+  authorization: {
+    enabled: true,
+    basic: {
+      users: [
+        {
+          username: "admin",
+          password: "admin",
+        },
+      ],
     },
-    "log": {
-        "debug": true,
-        "bunyan": {
-            "name": "Application",
-            "streams": [
-                {
-                    "level": "debug",
-                    "type": "rotating-file",
-                    "path": "logs/{hostname}.log",
-                    "period": "1d",
-                    "count": 2
-                }
-            ]
-        }
+  },
+  origin: {
+    ignoreExact: ["/"],
+    ignore: ["/doc/"],
+    require: {
+      application: true,
+      channel: true,
+      device: false,
     },
-    "authorization": {
-        "enabled": true,
-        "basic": {
-            "users": [
-                {
-                    "username": "admin",
-                    "password": "admin"
-                }
-            ]
-        }
+  },
+  db: {
+    mongodb: {
+      first: {
+        url: "mongodb://localhost:27017/sample-snf",
+        options: {
+          useNewUrlParser: true,
+        },
+      },
     },
-    "origin": {
-        "ignoreExact": [
-            "/"
-        ],
-        "ignore": [
-            "/doc/"
-        ],
-        "require": {
-            "application": true,
-            "channel": true,
-            "device": false
-        }
-    }
-}
+  },
+};
--- api/modules/sample-module/controller.js
+++ api/modules/sample-module/controller.js
@@ -1,35 +1,57 @@
-const { BaseController } = require('simple-node-framework').Base;
-const CustomerService = require('./service/customer-service');
+const { BaseController } = require("simple-node-framework").Base;
+const CustomerService = require("./service/customer-service");
 
 // sample controller
 class Controller extends BaseController {
-    constructor() {
-        super({
-            module: 'My Sample Controller' // the module name will prefix all your logs
-        });
-        this.service = new CustomerService();
+  constructor() {
+    super({
+      module: "My Sample Controller", // the module name will prefix all your logs
+    });
+    this.service = new CustomerService();
+  }
+
+  async load(req, res, next) {
+    super.activateRequestLog(req); // this will automatically put the request-id on all logs
+
+    const { name } = req.params;
+
+    try {
+      this.log.debug(`Loading customer [${name}]`);
+
+      const customer = await this.service.loadByName(name);
+
+      if (customer) res.send(200, customer);
+      else res.send(404);
+
+      return next();
+    } catch (error) {
+      this.log.error("Unexpected error on load", error);
+      res.send(500, "Unexpected error");
+      return next();
     }
+  }
 
-    async load(req, res, next) {
-        super.activateRequestLog(req); // this will automatically put the request-id on all logs
-
-        const { name } = req.params;
-
-        try {
-            this.log.debug(`Loading customer [${name}]`);
-
-            const customer = await this.service.loadByName(name);
-
-            if (customer) res.send(200, customer);
-            else res.send(404);
-
-            return next();
-        } catch (error) {
-            this.log.error('Unexpected error on load', error);
-            res.send(500, 'Unexpected error');
-            return next();
-        }
+  async list(req, res, next) {
+    try {
+      return res.json([
+        {
+          name: "any_name 001",
+        },
+        {
+          name: "any_name 002",
+        },
+        {
+          name: "any_name 003",
+        },
+        {
+          name: "any_name 004",
+        },
+      ]);
+    } catch (error) {
+      res.status(500).send("Unexpected error");
+      return next();
     }
+  }
 }
 
 module.exports = Controller;
