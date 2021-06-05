754332fd5e318ebf4c276b9ddda04f14046937fa
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-05 11:20:59 -0300

    fix: tests

--- api/modules/customers/test/unit/controller.unit.js
+++ api/modules/customers/test/unit/controller.unit.js
@@ -1,68 +1,68 @@
-const { TestHelper } = require('simple-node-framework');
-const sinon = require('sinon');
-const { expect } = require('chai');
-const Controller = require('../../controller');
+const { TestHelper } = require("simple-node-framework");
+const sinon = require("sinon");
+const { expect } = require("chai");
+const Controller = require("../../controller");
 
 const controller = new Controller();
 
-describe('Controller Test', () => {
-    const sandbox = sinon.createSandbox();
+describe("Controller Test", () => {
+  const sandbox = sinon.createSandbox();
 
-    before(() => {
-        TestHelper.before();
-    });
+  before(() => {
+    TestHelper.before();
+  });
 
-    after(() => {
-        TestHelper.after();
-    });
+  after(() => {
+    TestHelper.after();
+  });
 
-    afterEach(() => {
-        sandbox.restore();
-    });
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-    it('Should return 200', (done) => {
-        sandbox.stub(controller.service, 'loadByName').returns(
-            Promise.resolve({
-                name: 'some_name'
-            })
-        );
+  it("Should return 200", (done) => {
+    sandbox.stub(controller.service, "load").returns(
+      Promise.resolve({
+        name: "some_name",
+      })
+    );
 
-        const req = {
-            params: {
-                name: 'some_name'
-            }
-        };
+    const req = {
+      params: {
+        name: "some_name",
+      },
+    };
 
-        const res = {
-            send: (code) => {
-                expect(code).to.be.equal(200);
-                done();
-            }
-        };
+    const res = {
+      send: (code) => {
+        expect(code).to.be.equal(200);
+        done();
+      },
+    };
 
-        controller.load(req, res, sinon.spy()).catch(done);
-    });
+    controller.load(req, res, sinon.spy()).catch(done);
+  });
 
-    it('Should return customer', (done) => {
-        sandbox.stub(controller.service, 'loadByName').returns(
-            Promise.resolve({
-                name: 'some_name'
-            })
-        );
+  it("Should return customer", (done) => {
+    sandbox.stub(controller.service, "load").returns(
+      Promise.resolve({
+        name: "some_name",
+      })
+    );
 
-        const req = {
-            params: {
-                name: 'some_name'
-            }
-        };
+    const req = {
+      params: {
+        name: "some_name",
+      },
+    };
 
-        const res = {
-            send: (code, customer) => {
-                expect(customer.name).to.be.equal('some_name');
-                done();
-            }
-        };
+    const res = {
+      send: (code, customer) => {
+        expect(customer.name).to.be.equal("some_name");
+        done();
+      },
+    };
 
-        controller.load(req, res, sinon.spy()).catch(done);
-    });
+    controller.load(req, res, sinon.spy()).catch(done);
+  });
 });
